df6685ed72d6974a1c118bcad80b83f55c956703
SeungWonSong <sgouk504@gmail.com>
2020-10-29 16:13:01 +0900

    this is git test

---
+++ src/com/kh/gitTest/test.java
@@ -0,0 +1,13 @@
+package com.kh.gitTest;
+
+public class test {
+	public static String test() {
+		String a = "git Test!!";
+		return a;
+	}
+	public static void main(String[] args) {
+		String a = test();
+		System.out.println(a);
+	}
+
+}
