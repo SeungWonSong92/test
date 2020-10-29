55f91e7988f46c2e536cbf4e29fe9772ea14c220
SeungWonSong <sgouk504@gmail.com>
2020-10-29 16:37:19 +0900

    add gitignore

---
+++ src/com/kh/gitTest/test3.java
@@ -0,0 +1,13 @@
+package com.kh.gitTest;
+
+public class test3 {
+	public static String test() {
+		String a = "gitignore";
+		return a;
+	}
+	public static void main(String[] args) {
+		String a = test();
+		System.out.println(a);
+	}
+
+}
