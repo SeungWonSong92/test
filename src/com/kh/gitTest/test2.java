a13406b00ca3d4723c77da7a8d22ffe02df58435
SeungWonSong <sgouk504@gmail.com>
2020-10-29 16:26:32 +0900

    second commit

---
+++ src/com/kh/gitTest/test2.java
@@ -0,0 +1,13 @@
+package com.kh.gitTest;
+
+public class test2 {
+	public static String test() {
+		String a = "Second Commit Test";
+		return a;
+	}
+	public static void main(String[] args) {
+		String a = test();
+		System.out.println(a);
+	}
+
+}
